76e313f7155dabe1c49cc461ff1df0da1a4200e8
Raphael Eduardo de Oliveira <raphaeleo1@outlook.com>
2025-04-06 23:36:47 -0300

    parte 1

--- MauiAppTempoAgora/MainPage.xaml.cs
+++ MauiAppTempoAgora/MainPage.xaml.cs
@@ -1,4 +1,7 @@
-﻿namespace MauiAppTempoAgora
+﻿using MauiAppTempoAgora.Models;
+using MauiAppTempoAgora.Services;
+
+namespace MauiAppTempoAgora
 {
     public partial class MainPage : ContentPage
     {
@@ -8,17 +11,49 @@
         {
             InitializeComponent();
         }
-
-        private void OnCounterClicked(object sender, EventArgs e)
+        private async void Button_Clicked(object sender, EventArgs e)
         {
-            count++;
+            try
+            {
+                if (!string.IsNullOrEmpty(txt_cidade.Text))
+                {
+                    Tempo? t = await DataService.GetPrevisao(txt_cidade.Text);
 
-            if (count == 1)
-                CounterBtn.Text = $"Clicked {count} time";
-            else
-                CounterBtn.Text = $"Clicked {count} times";
 
-            SemanticScreenReader.Announce(CounterBtn.Text);
+
+
+                    if (t != null)
+                    {
+                        string dados_previsao = "";
+
+                        dados_previsao = $"Latitude: {t.lat} \n" +
+                                         $"Longitude: {t.lon} \n" +
+                                         $"Nascer do Sol: {t.sunrise} \n" +
+                                         $"Por do Sol: {t.sunset} \n" +
+                                         $"Temp Máx: {t.temp_max} \n" +
+                                         $"Temp min: {t.temp_min}\n" +
+                                         $"Descrição: {t.description} \n" +
+                                         $"Velociade do vento: {t.speed} \n" +
+                                         $"Visibilidade: {t.visibility} \n";
+
+                        lbl_res.Text = dados_previsao;
+                    }
+                    else
+                    {
+                        lbl_res.Text = "Cidade não encontrada, verifique se a escrita está correta.";
+                    }
+
+                }
+                else
+                {
+                    lbl_res.Text = "Preencha a cidade";
+                }
+
+            }
+            catch (Exception ex)
+            {
+                await DisplayAlert("Ops", ex.Message, "OK");
+            }
         }
     }
 
--- MauiAppTempoAgora/Models/Tempo.cs
+++ MauiAppTempoAgora/Models/Tempo.cs
@@ -2,16 +2,16 @@
 {
     public class Tempo
     {
-        public double lon {  get; set; }
-        public double lat { get; set; }
-        public double temp_min { get; set; }
-        public double temp_max { get; set; }
-        public int visibility { get; set; }
-        public double speed { get; set; }
-        public string main {  get; set; }
-        public string description { get; set; }
-        public int sunrise { get; set; }
-        public int sunset { get; set; }
+        public double? lon {  get; set; }
+        public double? lat { get; set; }
+        public double? temp_min { get; set; }
+        public double? temp_max { get; set; }
+        public int? visibility { get; set; }
+        public double? speed { get; set; }
+        public string? main {  get; set; }
+        public string? description { get; set; }
+        public string? sunrise { get; set; }
+        public string? sunset { get; set; }
 
     }
 }
